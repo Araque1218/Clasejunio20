3b6acee45322637e73dd3c5dd630feead169d0d8
Mateo_Araque34 <131192048+Araque1218@users.noreply.github.com>
2023-06-20 13:20:14 -0500

    Ultima parte

--- ejercicio.c
+++ ejercicio.c
@@ -12,3 +12,21 @@ struct persona {
 int main(int argc, char const *argv[]) {
     struct persona alumnos[4];
 
+    for (int i = 0; i < 4; i++) {
+        printf("Ingrese los datos del alumno %d:\n", i + 1);
+        printf("Matricula: ");
+        scanf("%d", &alumnos[i].matricula);
+        printf("Nombre: ");
+        scanf("%s", alumnos[i].nombre);
+        printf("Direccion: ");
+        scanf("%s", alumnos[i].direccion);
+        printf("Carrera: ");
+        scanf("%s", alumnos[i].carrera);
+        printf("Promedio: ");
+        scanf("%f", &alumnos[i].promedio);
+
+        fflush(stdin);
+    }
+
+    return 0;
+}
